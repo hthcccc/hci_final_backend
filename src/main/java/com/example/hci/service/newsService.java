df615611b8f926a8a68d4ccade6095b25f58aaba
hthcccc <65483560+hthcccc@users.noreply.github.com>
2022-06-13 00:34:14 +0800

    返回图片格式为BASE64

--- src/main/java/com/example/hci/service/newsService.java
+++ src/main/java/com/example/hci/service/newsService.java
@@ -15,11 +15,20 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Base64Utils;
+import sun.misc.BASE64Encoder;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.security.SecureRandom;
 import java.time.Instant;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class newsService {
@@ -44,6 +53,7 @@ public class newsService {
                 element.put("part",news1.getPart());
                 element.put("title",news1.getTitle());
                 element.put("url",news1.getUrl());
+                element.put("pict",image2Base64(news1.getUrl()));
                 result.add(element);
             }
             return ResultFactory.buildSuccessResult(result);
@@ -59,6 +69,7 @@ public class newsService {
                 element.put("part",news1.getPart());
                 element.put("title",news1.getTitle());
                 element.put("url",news1.getUrl());
+                element.put("pict",image2Base64(news1.getUrl()));
                 result.add(element);
             }
             return ResultFactory.buildSuccessResult(result);
@@ -86,6 +97,7 @@ public class newsService {
             element.put("part",news1.getPart());
             element.put("title",news1.getTitle());
             element.put("url",news1.getUrl());
+            element.put("pict",image2Base64(news1.getUrl()));
             result.add(element);
         }
         return ResultFactory.buildSuccessResult(result);
@@ -108,6 +120,7 @@ public class newsService {
             element.put("date",news1.getDate());
             element.put("title",news1.getTitle());
             element.put("url",news1.getUrl());
+            element.put("pict",image2Base64(news1.getUrl()));
             result.add(element);
         }
         return ResultFactory.buildSuccessResult(result);
@@ -128,6 +141,7 @@ public class newsService {
             element.put("date",news1.getDate());
             element.put("title",news1.getTitle());
             element.put("url",news1.getUrl());
+            element.put("pict",image2Base64(news1.getUrl()));
             result.add(element);
         }
         return ResultFactory.buildSuccessResult(result);
@@ -150,6 +164,7 @@ public class newsService {
             result.put("title",news.getTitle());
             result.put("date",news.getDate().toString());
             result.put("url",news.getUrl());
+            result.put("pict",image2Base64(news.getUrl()));
             result.put("id",news.getId());
             result.put("part",news.getPart());
             result.put("count",news.getCount());
@@ -182,6 +197,76 @@ public class newsService {
         news.setId(generateID(32));
         newsRepo.save(news);
     }
+
+    public static String image2Base64(String imgUrl) {
+        URL url = null;
+        InputStream is = null;
+        ByteArrayOutputStream outStream = null;
+        HttpURLConnection httpUrl = null;
+        try{
+            url = new URL(imgUrl);
+            httpUrl = (HttpURLConnection) url.openConnection();
+            httpUrl.connect();
+            httpUrl.getInputStream();
+            is = httpUrl.getInputStream();
+            outStream = new ByteArrayOutputStream();
+            //创建一个Buffer字符串
+            byte[] buffer = new byte[1024];
+            //每次读取的字符串长度，如果为-1，代表全部读取完毕
+            int len = 0;
+            //使用一个输入流从buffer里把数据读取出来
+            while( (len=is.read(buffer)) != -1 ){
+                //用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len代表读取的长度
+                outStream.write(buffer, 0, len);
+            }
+            // 对字节数组Base64编码
+            //return Base64Utils.encodeImage(outStream.toByteArray());
+            return Base64Utils.encodeToString(outStream.toByteArray());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            if(is != null)
+            {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(outStream != null)
+            {
+                try {
+                    outStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(httpUrl != null)
+            {
+                httpUrl.disconnect();
+            }
+        }
+        return imgUrl;
+    }
+
+    /**
+     * 图片转字符串
+     * @param image
+     * @return
+     */
+    public static String encodeImage(byte[] image){
+        BASE64Encoder decoder = new BASE64Encoder();
+        return replaceEnter(decoder.encode(image));
+    }
+
+    public static String replaceEnter(String str){
+        String reg ="[\n-\r]";
+        Pattern p = Pattern.compile(reg);
+        Matcher m = p.matcher(str);
+        return m.replaceAll("");
+    }
+
 
     public StringBuilder tryGetID(int length) {
         StringBuilder id=new StringBuilder();
